0a4f5f057d843554f50ff7060d454d1f9a6942ee
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2023-02-18 16:12:41 +0700

    Добавьте файлы проекта.

---
+++ CDEK/DBConnect.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Remoting.Contexts;
+
+namespace CDEK
+{
+	public class DBConnect
+	{
+		public static SqlConnection sqlConn;
+		public static void DataBaseConn(string sql)
+		{
+			sqlConn = new SqlConnection(sql);
+		}
+
+		public static bool DBC(string sql)
+		{
+			sqlConn = new SqlConnection(sql);
+			try
+			{
+				sqlConn.Open();
+				sqlConn.Close();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public static void LoadedDB(string sql,string nameTable, out DataTable dt)//предназначен при загрузке и выводе определенной таблицы
+		{
+			
+			sqlConn = new SqlConnection(sql);
+
+			sqlConn.Open();
+
+			string Get_Data = "SELECT * FROM [" + nameTable + "]";
+
+			SqlCommand cmd = sqlConn.CreateCommand();
+			cmd.CommandText = Get_Data;
+
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			dt = new DataTable("[" + nameTable + "]");
+
+			adapter.Fill(dt);
+		}
+
+		public static void DateFilter(string sql, string nameTable , string Date, out DataTable dt)//фильтр по дате
+		{
+			sqlConn = new SqlConnection(sql);
+
+			sqlConn.Open();
+
+			string Get_Data = "SELECT * FROM [" + nameTable + "] WHERE DATE = " + "'" + Date + "'";
+			SqlCommand cmd = sqlConn.CreateCommand();
+			cmd.CommandText = Get_Data;
+
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			dt = new DataTable("[" + nameTable + "]");
+
+			adapter.Fill(dt);
+		}
+
+		public static void EventFilter(string sql, string nameTable, string nameEvent, out DataTable dt)//фильтр по событиям
+		{
+			sqlConn = new SqlConnection(sql);
+
+			sqlConn.Open();
+
+			string Get_Data = "SELECT * FROM [" + nameTable + "] WHERE [Событие] = " + "'" + nameEvent + "'";
+			SqlCommand cmd = sqlConn.CreateCommand();
+			cmd.CommandText = Get_Data;
+
+			SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+			dt = new DataTable("[" + nameTable + "]");
+
+			adapter.Fill(dt);
+		}
+
+		public static void SQLQueryAutorization(string id, string password, string nameTable, string nameColumnMail, string nameColumnPassword, out bool Autorization)//отправление sql-запросов на авторизацию
+		{
+			string sqlQuery = "SELECT * FROM [dbo].[" + nameTable + "] WHERE [" + nameColumnMail + "]= '" + id + "' and [" + nameColumnPassword + "]='" + password + "'";
+			
+			using (SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlQuery, sqlConn))
+			{
+				DataTable table = new DataTable();
+
+				dataAdapter.Fill(table);
+
+				if (table.Rows.Count == 0)
+				{
+					Autorization = false;
+				}
+				else
+				{
+					Autorization = true;
+				}
+			}
+		}
+	}
+}
---
+++ CDEK/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CDEK
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Метод MainWindow предназначен для инициализации компонентов.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Метод btnSetApplication_Click предназначен для открытия окна подачи заявок пользователя
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e">Содержит информацию о состоянии и данные события</param>
+		private void btnSetApplication_Click(object sender, RoutedEventArgs e)
+		{
+			ApplicationWin win = new ApplicationWin();
+			win.Show();
+			this.Close();
+		}
+
+		/// <summary>
+		/// Метод Autorization_Loaded предназначен для того, чтобы при загрузке окна происходило подключение к базе данных.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e">Содержит информацию о состоянии и данные события</param>
+		private void Autorization_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				string sqlConn = @"Data Source = DESKTOP-R2UPGH3\DR; Initial Catalog = CDEK; Integrated Security = True";
+				DBConnect.DataBaseConn(sqlConn);
+			}
+			catch
+			{
+				MessageBox.Show("No connection");
+			}
+		}
+
+		/// <summary>
+		/// Метод btnSignIn_Click предназначен для того, чтобы менеджер мог зайти на окно заявок
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e">Содержит информацию о состоянии и данные события</param>
+		private void btnSignIn_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				bool Autorization;
+				DBConnect.SQLQueryAutorization(tbLogin.Text, pbManager.Password, "ManagerTable", "Login", "Password", out Autorization);
+				if(Autorization)
+				{
+					ViewApplicationsWin applicationsWin = new ViewApplicationsWin();
+					applicationsWin.Show();
+					this.Close();
+				}
+				else
+				{
+					MessageBox.Show("Login or password entered incorrectly");
+				}
+			}
+			catch
+			{
+				MessageBox.Show("No connection or incorrect data entered");
+			}
+		}
+
+		private void pbManager_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.Key == Key.Enter)
+			{
+				btnSetApplication_Click(sender, e);
+			}
+		}
+	}
+}
